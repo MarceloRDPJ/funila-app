7ca05eb18275f5d0b558912506d261df01d4c5bf
Marcelo Rodrigues dos Passos Junior <marceloprego1223@gmail.com>
2026-02-19 16:58:57 -0300

    Update API_URL to point to the new backend

--- frontend/admin/js/auth.js
+++ frontend/admin/js/auth.js
@@ -1,7 +1,7 @@
 const SUPABASE_URL  = "https://qitbyswmidyakadrzatz.supabase.co";  // ← substitua
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFpdGJ5c3dtaWR5YWthZHJ6YXR6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE1MDQ3NzAsImV4cCI6MjA4NzA4MDc3MH0.FHUD9EuHNOnxv7UALkHNlLiEZv5Q7yYvT9GIz3QeSl0";            // ← substitua
 // Use relative path since we are serving frontend from backend
-const API_URL = "";
+const API_URL = "https://funila-app.onrender.com";
 
 let supabaseClient = null;
 
